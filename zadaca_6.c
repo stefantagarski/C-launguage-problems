58ee0cdb58d80bfa1de4336864fb14f55e36859b
Stefan Tagarski <116569281+stefantagarski@users.noreply.github.com>
2022-12-08 19:52:36 +0100

    Add files via upload

---
+++ zadaca_6.c
@@ -0,0 +1,46 @@
+//Од тастатура се внесуваат должини на три отсечки во произволен редослед. Да се провери дали од
+//дадените отсечки може да се конструра триаголник.
+//Ако може, да се испечати дали триаголникот е разностран, рамностран или рамнокрак и да му се пресмета плоштината.
+#include<stdio.h>
+
+int main() {
+    float a, b, c;
+    scanf("%f%f%f", &a, &b, &c);
+
+    //Dali moze voopsto da formirame triagolnik?
+    if ((a + b) > c && (a + c) > b && (b + c) > a) {
+        //da, moze da formirame triagolnik.
+        //5,2,3 --> 2,5,3 --> 2,3,5  --> 2,3,5
+        float tmp;
+        if (a > b) {
+            tmp = b; //tmp = 2
+            b = a; //b=3
+            a = tmp;//a=2
+        }
+        if (b > c) {
+            tmp = c;
+            c = b;
+            b = tmp;
+        }
+        if (a > c) {
+            tmp = a;
+            a = c;
+            c = tmp;
+        }
+        //raznostran
+        if (a != b && b != c && a != c) {
+            printf("Raznostran");
+        }//ramnostran
+        else if (a == b && b == c && a == c) { //1==c
+            printf("Ramnostran");
+        } else { //ramnokrak
+            printf("Ramnokrak");
+        }
+
+
+    } else {
+        printf("Can't form a triangle from the sides %.2f, %.2f, %.2f", a, b, c);
+    }
+
+    return 0;
+}
